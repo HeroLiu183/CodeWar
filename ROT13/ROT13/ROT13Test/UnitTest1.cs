51e3f0dcf4f079a32f373cbecd97fd13adbfdd7a
HeroLiu183 <heroliu183@gmail.com>
2018-05-17 11:26:37 +0800

    just commit

--- ROT13/ROT13/ROT13Test/UnitTest1.cs
+++ ROT13/ROT13/ROT13Test/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿using Encoder;
+﻿using System;
+using System.Text;
+using Encoder;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ROT13Test
@@ -14,7 +16,7 @@ namespace ROT13Test
             var result = _encodeHelper.ROTEncode("a");
             var expect = "n";
 
-            Assert.AreEqual(expect,result);
+            Assert.AreEqual(expect, result);
         }
 
         [TestMethod]
@@ -31,7 +33,54 @@ namespace ROT13Test
         {
             var result = _encodeHelper.ROTEncode("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
             var expect = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-            Assert.AreEqual(expect,result);
+            Assert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void test()
+        {
+            var a = new StringBuilder();
+            a.Append("123;");
+            a.Append("123;");
+            a.Append("123;");
+            a.Append("123;");
+            a.Append("123;");
+            a.Append("113;");
+
+            a.Remove(a.Length - 1, 1);
+            Console.WriteLine(a.ToString());
+        }
+
+        [TestMethod]
+        public void now()
+        {
+            var a = new Mytest();
+            Console.WriteLine(a.q);
+            Console.WriteLine(a.ba);
+            a.ba = 100;
+            a.q = 101;
+            Console.WriteLine(a.q);
+            a.k(1000);
+        }
+    }
+
+    public class Mytest : Testk
+    {
+        public void k(int dd) => g8(dd);
+        public override int q => ba;
+        public int ba { get; set; }
+    }
+
+    public class Testk
+    {
+        public virtual void g8(int i)
+        {
+            Console.WriteLine(i);
+        }
+        private int temp = 99;
+        public virtual int q {
+            get { return temp; }
+            set { temp = value; }
         }
     }
 }
